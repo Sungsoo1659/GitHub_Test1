63a54405b965ec2006bf1e635046bdc3afef64dd
KALO <sungtn1118@gmail.com>
2025-01-07 10:57:44 +0900

    Initial Commit

---
+++ GitHub_Test1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub_Test1
+//
+//  Created by 아이티젠 on 1/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
